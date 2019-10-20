731a8d4d459ea618ad93bee4d8cc7a9d59f6e27c
daniel.silver <daniel@silverengineered.com>
2019-10-20 15:46:57 -0400

    added new bow

--- bow.py
+++ bow.py
@@ -1,23 +1,18 @@
 from gensim import corpora
 from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
 
-vectorizer = TfidfVectorizer()
-newsgroups = fetch_20newsgroups(subset='train')
-newsgroups_test = fetch_20newsgroups(subset='test')
-texts = list(newsgroups.data)
-targets = newsgroups.target
-
-vectors = vectorizer.fit_transform(texts) #TFIDF
-vectors_test = vectorizer.transform(newsgroups_test.data)
-
-#Naive Bayes classifier
-clf = MultinomialNB(alpha=.01)
-clf.fit(vectors, targets)
-
-pred = clf.predict(vectors_test)
-#Get accuracy
-acc = metrics.f1_score(newsgroups_test.target, pred, average='macro')
-print(acc)
+def get_bow(cleaned=False):
+	vectorizer = CountVectorizer()
+	if cleaned:
+		newsgroups = fetch_20newsgroups(remove=('headers', 'footers'))
+	else:
+		newsgroups = fetch_20newsgroups()
+	texts = list(newsgroups.data)
+	bow = vectorizer.fit_transform(texts)
+	return bow
