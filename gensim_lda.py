bb6a77041df8664cd84dadae5b49649eeb61214b
Matthew Hoffman <mhoffman2016@hotmail.com>
2019-10-20 23:45:00 -0400

    gooooo

--- gensim_lda.py
+++ gensim_lda.py
@@ -6,12 +6,12 @@ import pandas as pd
 
 
 
-documents = fetch_20newsgroups().data
+documents = fetch_20newsgroups(remove=('headers','footers')).data
 texts = [[word for word in document.lower().split()] for document in documents]
 dictionary = corpora.Dictionary(texts)
 corpus = [dictionary.doc2bow(text) for text in texts]
 
 
 lda_model = LdaModel(corpus=corpus, id2word = dictionary, num_topics=20)
-#vis = pyLDAvis.gensim.prepare(lda_model, corpus, dictionary=lda_model.id2word)
-#pyLDAvis.enable_notebook()
+vis = pyLDAvis.gensim.prepare(lda_model, corpus, dictionary=lda_model.id2word)
+pyLDAvis.save_html(vis, 'LDA_Visualization.html')
